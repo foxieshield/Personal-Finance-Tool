274f7b6bfc7ce20497514308d465226732ad03c6
foxieshield <jhoysteve@yahoo.com.mx>
2026-08-29 22:49:39 -0600

    Implement add movement button functionality
    Added event listener for adding movement details.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const movementNameInput = document.getElementById("movementName");
+const movementDateInput = document.getElementById("movementDate");
+const movementAmountInput = document.getElementById("movementAmount");
+
+const movementAccountInput = document.getElementById("movementAccount");
+const movementTypeInput = document.getElementById("movementType");
+
+const addMovementButton = document.getElementById("addMovementButton");
+
 const creditLimitInput = document.getElementById("creditLimit");
 const creditDebtInput = document.getElementById("creditDebt");
 const debitMoneyInput = document.getElementById("debitMoney");
@@ -7,7 +16,7 @@ const currentStockDisplay = document.getElementById("currentStock");
 const availableCreditDisplay = document.getElementById("availableCredit");
 
 calculateButton.addEventListener("click", function () {
-
+    
     const creditLimit = Number(creditLimitInput.value);
     const creditDebt = Number(creditDebtInput.value);
     const debitMoney = Number(debitMoneyInput.value);
@@ -21,5 +30,20 @@ calculateButton.addEventListener("click", function () {
     console.log("Current Stock:", currentStock);
     
     currentStockDisplay.textContent = "$" + currentStock.toFixed(2);
+
+});
+addMovementButton.addEventListener("click", function () {
+
+    const name = movementNameInput.value;
+    const date = movementDateInput.value;
+    const amount = Number(movementAmountInput.value);
+    const account = movementAccountInput.value;
+    const type = movementTypeInput.value;
+
+    console.log("Name:", name);
+    console.log("Date:", date);
+    console.log("Amount:", amount);
+    console.log("Account:", account);
+    console.log("Type:", type);
 
 });
